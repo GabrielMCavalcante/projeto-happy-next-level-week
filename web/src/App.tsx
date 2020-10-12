ea01d109341d1d106602a5a52f7ceca8d9ed4d69
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-12 12:59:26 -0400

    Move file content to pages folder (Landing)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,36 +1,10 @@
 import React from 'react'
-import { FiArrowRight } from 'react-icons/fi'
 
-// Images
-import appLogo from 'assets/images/logo.svg'
-
-// Styles
-import 'assets/styles/pages/landing.css'
+// Routes
+import Routes from 'routes'
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={appLogo} alt="Logo Happy" />
-
-        <main>
-          <div id="main-heading">
-            <h1>Leve felicidade para o mundo</h1>
-            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-          </div>
-
-          <div className="location">
-            <strong>Manaus</strong>
-            <span>Amazonas</span>
-          </div>
-        </main>
-
-        <a href="go" className="enter-app">
-          <FiArrowRight size={32} color="#8D734B" />
-        </a>
-      </div>
-    </div>
-  )
+  return <Routes />
 }
 
 export default App
