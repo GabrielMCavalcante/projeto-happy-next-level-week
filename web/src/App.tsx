75d9ca2093d0505b71ea9e5e60ebd12658ef4879
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-12 12:02:47 -0400

    Add enter-app anchor icon

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FiArrowRight } from 'react-icons/fi'
 
 // Images
 import appLogo from 'assets/images/logo.svg'
@@ -11,7 +12,21 @@ function App() {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
+        <img src={appLogo} alt="Logo Happy" />
 
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+
+          <div className="location">
+            <strong>Manaus</strong>
+            <span>Amazonas</span>
+          </div>
+        </main>
+
+        <a href="go" className="enter-app">
+          <FiArrowRight size={32} color="#8D734B"/>
+        </a>
       </div>
     </div>
   )
