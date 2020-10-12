8740883074706a64c202f5bb806b0aa02692c5cf
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-12 11:33:01 -0400

    Import landing-kids svg file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Images
 import appLogo from 'assets/images/logo.svg'
+import landingKids from 'assets/images/landing-kids.svg'
 
 // Styles
 import 'assets/styles/pages/landing.css'
