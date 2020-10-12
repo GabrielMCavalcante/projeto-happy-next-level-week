af931597041b3a9c9329dcb51f5d4f5a2fe1e7e4
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-12 17:54:22 -0400

    Add map component

--- web/src/pages/OrphanageMap.tsx
+++ web/src/pages/OrphanageMap.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
 
@@ -7,8 +8,16 @@ import happyFace from 'assets/images/happy-face-logo.svg'
 
 // Styles
 import 'assets/styles/pages/orphanageMap.css'
+import 'leaflet/dist/leaflet.css'
 
 function OrphanageMap() {
+
+    const [lat, setLat] = useState(-3.0828679)
+    const [lng, setLng] = useState(-60.0115209)
+    const [zoom, setZoom] = useState(15)
+
+    const position: [number, number] = [lat, lng]
+
     return (
         <div id="page-map">
             <aside>
@@ -25,8 +34,26 @@ function OrphanageMap() {
                 </footer>
             </aside>
 
+            <Map 
+                center={position} 
+                zoom={zoom} 
+                style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                <TileLayer 
+                    url={process.env.API_KEY!}
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </Map>
+
             <Link to="/" id="create-orphanage-btn">
-                <FiPlus size={32} color="#FFFFFF"/>
+                <FiPlus size={32} color="#FFFFFF" />
             </Link>
         </div>
     )
