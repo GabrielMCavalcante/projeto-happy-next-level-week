fbb0f695b6d05e48c548b149f58d1d0f81fed214
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-13 14:39:20 -0400

    Add static folder to serve images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import routes from './routes'
+import path from 'path'
 import './database/connection'
 
 const app = express()
 const PORT = 3333
 
 app.use(express.json())
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(routes)
 
 app.listen(PORT, () => console.log("Server started @PORT", PORT))
