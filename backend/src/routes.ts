b099bbb907cf7a0c2b2bd5daec799df5e606241e
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-13 10:42:18 -0400

    Add post resource for orphanages creation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,55 @@
 import { Router } from 'express'
+import { getRepository } from 'typeorm'
+import OrphanageModel from './models/Orphanage'
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-    res.send("Hello, World!")
+routes.post('/orphanages', async (req, res) => {
+    const { 
+        name, 
+        about,
+        latitude, 
+        longitude,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = req.body
+
+    const orphanagesRepository = getRepository(OrphanageModel)
+    
+    const arrayOfEmptyAttributes = []
+    for (const attr in req.body) {
+        if (req.body[attr] === null || req.body[attr] === "") {
+            arrayOfEmptyAttributes.push(attr)
+        }
+    }
+
+    if (arrayOfEmptyAttributes.length > 0) {
+        return res.status(400).json({
+            status: 400,
+            type: "Required field empty or null",
+            message: "The following fields are required: "
+            + arrayOfEmptyAttributes.join(", ")
+        })
+    }
+
+    const createdOrphanage = orphanagesRepository.create({
+        name, 
+        about,
+        latitude, 
+        longitude,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    })
+
+    await orphanagesRepository.save(createdOrphanage)
+
+    return res.status(200).json({
+        status: 200,
+        type: "Post successfull",
+        message: "Orphanage created successfully."
+    })
 })
 
 export default routes
