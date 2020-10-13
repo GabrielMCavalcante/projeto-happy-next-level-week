b60488cc7cc32dbc0c30083fd645fb35eef69155
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-13 11:55:12 -0400

    Add resource to get list of orphanages

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ import checkEmptyOrNullParams from './middlewares/checkEmptyOrNullParams'
 const routes = Router()
 
 // Orphanages
+routes.get('/orphanages', OrphanagesController.index)
 routes.post('/orphanages', checkEmptyOrNullParams, OrphanagesController.create)
 
 export default routes
