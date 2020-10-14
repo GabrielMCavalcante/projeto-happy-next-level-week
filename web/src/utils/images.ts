9d1a4106dfda3c2947c13a9ad1e8279297114931
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-14 11:50:22 -0400

    Create images barrel import util file

---
+++ web/src/utils/images.ts
@@ -0,0 +1,9 @@
+import happyFaceLogo from '../assets/images/happy-face-logo.svg'
+import landingKidsImg from '../assets/images/landing-kids.svg'
+import appLogo from '../assets/images/logo.svg'
+
+export {
+    happyFaceLogo,
+    landingKidsImg,
+    appLogo
+}
