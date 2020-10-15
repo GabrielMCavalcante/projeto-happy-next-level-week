278bdf5313fa3d9d422c6b877735b62aac0cb9ca
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-15 14:17:04 -0400

    Add map

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, Dimensions, StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import { View, Text, Image, Dimensions, StyleSheet } from 'react-native'
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'
 import * as Location from 'expo-location'
 import { AppLoading } from 'expo'
+import { Entypo } from '@expo/vector-icons'
+import { RectButton } from 'react-native-gesture-handler'
+import HappyFaceLogo from 'assets/images/happy-face-logo.png'
 
 function OrphanagesMap() {
   const [location, setLocation] = useState<[number, number]>([0, 0])
@@ -26,16 +29,49 @@ function OrphanagesMap() {
         location[0] !== 0
           && location[1] !== 0
           ? (
-            <MapView
-              initialRegion={{
-                latitude: location[0],
-                longitude: location[1],
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-              }}
-              minZoomLevel={15}
-              style={styles.mapStyle}
-            />
+            <>
+              <MapView
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{
+                  latitude: location[0],
+                  longitude: location[1],
+                  latitudeDelta: 0.008,
+                  longitudeDelta: 0.008
+                }}
+                style={styles.mapStyle}
+              >
+                <Marker
+                  coordinate={{
+                    latitude: location[0],
+                    longitude: location[1]
+                  }}
+                  calloutAnchor={{
+                    x: 0.7,
+                    y: -0.1
+                  }}
+                >
+                  <Image
+                    source={HappyFaceLogo}
+                    style={{ width: 60, height: 60 }}
+                    resizeMode="contain"
+                  />
+                  <Callout tooltip onPress={() => alert("oi")}>
+                    <View style={styles.calloutContainer}>
+                      <Text style={styles.calloutText}>
+                        Lar das Meninas
+                      </Text>
+                    </View>
+                  </Callout>
+                </Marker>
+              </MapView>
+
+              <View style={styles.footer}>
+                <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+                <RectButton style={styles.addBtn}>
+                  <Entypo name="plus" size={24} color="#FFFFFF" />
+                </RectButton>
+              </View>
+            </>
           ) : <AppLoading />
       }
     </View>
@@ -47,6 +83,55 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
+
+  calloutContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 140,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 10,
+    borderColor: "rgba(0,137,165, 0.4)",
+    borderWidth: 1
+  },
+
+  calloutText: {
+    fontFamily: 'Nunito_700Bold',
+    fontSize: 14,
+    color: '#0089A5'
+  },
+
+  footer: {
+    width: 327,
+    height: 56,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    bottom: 10,
+    borderRadius: 20,
+    paddingLeft: 25,
+    elevation: 3
+  },
+
+  footerText: {
+    color: '#8FA7B3',
+    fontSize: 15,
+    fontFamily: 'Nunito_700Bold'
+  },
+
+  addBtn: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15C3D6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
 
 export default OrphanagesMap
