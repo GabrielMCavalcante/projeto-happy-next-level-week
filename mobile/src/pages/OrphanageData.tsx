84f6c4895436886ae0d50689bc25f6a61d9db27e
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-16 16:08:42 -0400

    Add form data state storing and image picker

--- mobile/src/pages/OrphanageData.tsx
+++ mobile/src/pages/OrphanageData.tsx
@@ -1,9 +1,58 @@
-import React from "react"
-import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity } from "react-native"
+import React, { useState, useEffect } from "react"
+import {
+  View,
+  Text,
+  Image,
+  Switch,
+  Platform,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native"
 import { Entypo } from "@expo/vector-icons"
+import * as ImagePicker from 'expo-image-picker'
 import { RectButton } from "react-native-gesture-handler"
 
 export default function OrphanageData() {
+
+  const [name, setName] = useState("")
+  const [about, setAbout] = useState("")
+  const [instructions, setInstructions] = useState("")
+  const [images, setImages] = useState<string[]>([])
+  const [opening_hours, setOpeningHours] = useState("")
+  const [open_on_weekends, setOpenOnWeekends] = useState(false)
+
+  useEffect(() => {
+    (async function () {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestCameraRollPermissionsAsync()
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!')
+        }
+      }
+    })()
+  }, [])
+
+  async function handleCreateOrphanage() {
+    // ...
+  }
+
+  async function handleImageSelection() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    })
+
+    if (!result.cancelled) {
+      setImages([...images, (result as any).uri])
+    }
+  }
+
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
       <Text style={styles.title}>Dados</Text>
@@ -11,23 +60,35 @@ export default function OrphanageData() {
       <Text style={styles.label}>Nome</Text>
       <TextInput
         style={styles.input}
+        value={name}
+        onChangeText={text => setName(text)}
       />
 
       <Text style={styles.label}>Sobre</Text>
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={about}
+        onChangeText={text => setAbout(text)}
       />
 
-      <Text style={styles.label}>Whatsapp</Text>
+      {/* <Text style={styles.label}>Whatsapp</Text>
       <TextInput
         style={styles.input}
-      />
+      /> */}
 
       <Text style={styles.label}>Fotos</Text>
-      <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
-        <Entypo name="plus" size={24} color="#15B6D6" />
-      </TouchableOpacity>
+      <View style={styles.imagesContainer}>
+        {
+          images.map((image, i) => (
+            <Image key={i} style={styles.selectedImage} source={{ uri: image }} />
+          ))
+        }
+
+        <TouchableOpacity style={styles.imagesInput} onPress={handleImageSelection}>
+          <Entypo name="plus" size={24} color="#15B6D6" />
+        </TouchableOpacity>
+      </View>
 
       <Text style={styles.title}>Visitação</Text>
 
@@ -35,22 +96,28 @@ export default function OrphanageData() {
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={instructions}
+        onChangeText={text => setInstructions(text)}
       />
 
       <Text style={styles.label}>Horario de visitas</Text>
       <TextInput
         style={styles.input}
+        value={opening_hours}
+        onChangeText={text => setOpeningHours(text)}
       />
 
       <View style={styles.switchContainer}>
         <Text style={styles.label}>Atende final de semana?</Text>
-        <Switch 
-          thumbColor="#fff" 
+        <Switch
+          thumbColor="#fff"
           trackColor={{ false: "#ccc", true: "#39CC83" }}
+          value={open_on_weekends}
+          onValueChange={() => setOpenOnWeekends(!open_on_weekends)}
         />
       </View>
 
-      <RectButton style={styles.nextButton} onPress={() => {}}>
+      <RectButton style={styles.nextButton} onPress={handleCreateOrphanage}>
         <Text style={styles.nextButtonText}>Cadastrar</Text>
       </RectButton>
     </ScrollView>
@@ -95,16 +162,33 @@ const styles = StyleSheet.create({
     textAlignVertical: "top",
   },
 
+  imagesContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    width: Dimensions.get("window").width,
+    marginBottom: 10
+  },
+
+  selectedImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 20,
+    marginRight: 10,
+    marginBottom: 10
+  },
+
   imagesInput: {
     backgroundColor: "rgba(255, 255, 255, 0.5)",
     borderStyle: "dashed",
     borderColor: "#96D2F0",
     borderWidth: 1.4,
     borderRadius: 20,
-    height: 56,
+    width: 70,
+    height: 70,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 32,
   },
 
   switchContainer: {
