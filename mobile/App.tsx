d58685b63f263c31efa634796a40c0081eec5220
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-16 08:01:03 -0400

    Add conditional rendering of routes/onboarding

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import { AppLoading } from 'expo'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
+import AsyncStorage from "@react-native-community/async-storage"
 import {
   useFonts,
   Nunito_400Regular,
@@ -12,22 +13,43 @@ import {
 
 // Routes
 import Routes from 'routes'
+import Onboarding from 'pages/Onboarding'
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
+  const [route, setRoute] = useState(Routes)
+
+  function dismissOnboarding() {
+    AsyncStorage.setItem("happy:app:alreadyopened", "true")
+      .then(() => setRoute(Routes))
+  }
+
+  const [fontsLoaded] = useFonts({
     Nunito_400Regular,
     Nunito_600SemiBold,
     Nunito_700Bold,
     Nunito_800ExtraBold
   })
 
+  useEffect(() => {
+    (async function () {
+      if (fontsLoaded) {
+        const check = await AsyncStorage.getItem("happy:app:alreadyopened")
+        if (!check || check === "false") {
+          setRoute(<Onboarding onDismiss={dismissOnboarding} />)
+        } else {
+          await AsyncStorage.setItem("happy:app:alreadyopened", "false")
+        }
+      }
+    })()
+  }, [fontsLoaded])
+
   if (!fontsLoaded) {
     return <AppLoading />
   } else {
     return (
       <View style={styles.container}>
-        <StatusBar style="auto" hidden/>
-        <Routes />
+        <StatusBar style="auto" hidden />
+        { route}
       </View>
     )
   }
