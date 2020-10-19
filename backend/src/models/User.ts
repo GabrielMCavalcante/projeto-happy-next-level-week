e11f865704a72a254777917c944812e95982e3fe
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-19 12:04:02 -0400

    Add on-to-one relationship with recovery_tokens model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,13 +1,18 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm"
+import PasswordRecoveryTokens from "./PasswordRecoveryTokens"
 
 @Entity("users")
 export default class User {
     @PrimaryGeneratedColumn("increment")
     id: number;
 
-    @Column()
+    @Column({ unique: true })
     email: string;
 
     @Column()
     password: string;
+
+    @OneToOne(() => PasswordRecoveryTokens)
+    @JoinColumn()
+    password_recovery_token: PasswordRecoveryTokens;
 }
