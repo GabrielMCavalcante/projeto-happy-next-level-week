efdad8c461ef1eb33d1f79119243f5a2ae9dc76a
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-19 16:08:27 -0400

    Add password-recovery-related routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,26 +1,30 @@
-import { Router } from 'express'
-import OrphanagesController from './controllers/orphanages'
-import multer from 'multer'
-import multerConfig from './config/upload'
+import { Router } from "express"
+import multer from "multer"
+import multerConfig from "./config/upload"
 
 const upload = multer(multerConfig)
 
+// Controllers
+import OrphanagesController from "./controllers/orphanages"
+import AuthController from "./controllers/authentication"
+
 // Middlewares
-import checkEmptyOrNullParams from './middlewares/checkEmptyOrNullParams'
-import AuthenticationController from './controllers/authentication'
+import checkEmptyOrNullParams from "./middlewares/checkEmptyOrNullParams"
 
 const routes = Router()
 
 // Orphanages
-routes.get('/orphanages', OrphanagesController.index)
-routes.get('/orphanages/:id', OrphanagesController.findById)
-routes.post('/orphanages', 
+routes.get("/orphanages", OrphanagesController.index)
+routes.get("/orphanages/:id", OrphanagesController.findById)
+routes.post("/orphanages", 
     checkEmptyOrNullParams, 
     upload.array("images"), 
 OrphanagesController.create)
 
 // Authentication
-routes.post("/auth/signup", AuthenticationController.signup)
-routes.post("/auth/signin", AuthenticationController.signin)
+routes.post("/auth/signup", AuthController.signup)
+routes.post("/auth/signin", AuthController.signin)
+routes.post("/auth/password-recovery/request-token", AuthController.requestRecoveryToken)
+routes.put("/auth/password-recovery/reset-password", AuthController.resetPassword)
 
 export default routes
