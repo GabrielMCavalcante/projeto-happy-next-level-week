566d86cfedc8ac892fff9f8aa864dcba4f5b42d8
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-20 10:29:13 -0400

    Fix space indentation size to 2

--- backend/src/views/dashboard.ts
+++ backend/src/views/dashboard.ts
@@ -2,15 +2,15 @@ import Orphanage from "../models/Orphanage"
 
 export default {
   render(orphanage: Orphanage) {
-      return {
-          id: orphanage.id,
-          name: orphanage.name,
-          latitude: orphanage.latitude,
-          longitude: orphanage.longitude
-      }
+    return {
+      id: orphanage.id,
+      name: orphanage.name,
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude
+    }
   },
 
   renderMany(orphanages: Orphanage[]) {
-      return orphanages.map(orphanage => this.render(orphanage))
+    return orphanages.map(orphanage => this.render(orphanage))
   }
 }
