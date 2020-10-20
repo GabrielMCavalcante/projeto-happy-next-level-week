1b69f86fe967f703db6256baa6f27d5433e1db77
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-20 09:02:11 -0400

    Create initial structure for dashboard controller

---
+++ backend/src/controllers/dashboard.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+
+export default class DashboardController {
+  static async index(req: Request, res: Response) {
+
+  }
+
+  static async indexPending(req: Request, res: Response) {
+
+  }
+
+  static async update(req: Request, res: Response) {
+
+  }
+
+  static async updatePending(req: Request, res: Response) {
+
+  }
+
+  static async delete(req: Request, res: Response) {
+
+  }
+}
