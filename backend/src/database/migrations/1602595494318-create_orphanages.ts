e79df398d48e635276aee6ecba3b4963fee94c0d
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-20 07:43:48 -0400

    Add pending_approval property

--- backend/src/database/migrations/1602595494318-create_orphanages.ts
+++ backend/src/database/migrations/1602595494318-create_orphanages.ts
@@ -1,63 +1,68 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class createOrphanages1602595494318 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table({
-            name: "orphanages",
-            columns: [
-                {
-                    name: "id",
-                    type: "integer",
-                    unsigned: true,
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: "increment"
-                },
-                {
-                    name: "name",
-                    type: "varchar",
-                    isNullable: false
-                },
-                {
-                    name: "latitude",
-                    type: "decimal",
-                    scale: 10,
-                    precision: 2,
-                    isNullable: false
-                },
-                {
-                    name: "longitude",
-                    type: "decimal",
-                    scale: 10,
-                    precision: 2,
-                    isNullable: false
-                },
-                {
-                    name: "about",
-                    type: "text",
-                    isNullable: false
-                },
-                {
-                    name: "instructions",
-                    type: "text",
-                    isNullable: false
-                },
-                {
-                    name: "open_on_weekends",
-                    type: "boolean",
-                    default: false
-                },
-                {
-                    name: "opening_hours",
-                    type: "varchar",
-                    isNullable: false
-                }
-            ]
-        }))
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: "orphanages",
+      columns: [
+        {
+          name: "id",
+          type: "integer",
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: "increment"
+        },
+        {
+          name: "name",
+          type: "varchar",
+          isNullable: false
+        },
+        {
+          name: "latitude",
+          type: "decimal",
+          scale: 10,
+          precision: 2,
+          isNullable: false
+        },
+        {
+          name: "longitude",
+          type: "decimal",
+          scale: 10,
+          precision: 2,
+          isNullable: false
+        },
+        {
+          name: "about",
+          type: "text",
+          isNullable: false
+        },
+        {
+          name: "instructions",
+          type: "text",
+          isNullable: false
+        },
+        {
+          name: "open_on_weekends",
+          type: "boolean",
+          default: false
+        },
+        {
+          name: "opening_hours",
+          type: "varchar",
+          isNullable: false
+        },
+        {
+          name: "pending_approval",
+          type: "boolean",
+          default: true
+        }
+      ]
+    }))
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("orphanages")
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("orphanages")
+  }
 
 }
