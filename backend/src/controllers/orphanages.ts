dd734936019ee19f6c0e70ceabe1bfd2414f9855
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-20 08:54:23 -0400

    Add missing parameters checker

--- backend/src/controllers/orphanages.ts
+++ backend/src/controllers/orphanages.ts
@@ -5,98 +5,135 @@ import OrphanageModel from '../models/Orphanage'
 import OrphanageView from '../views/orphanages'
 
 export default class OrphanagesController {
-    static async index(req: Request, res: Response) {
-        const orphanagesRepository = getRepository(OrphanageModel)
+  static async index(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(OrphanageModel)
 
-        const fetchedOrphanages = await orphanagesRepository.find({
-            relations: ["images"],
-            where: {
-                pending_approval: false
-            }
-        })
+    const fetchedOrphanages = await orphanagesRepository.find({
+      relations: ["images"],
+      where: {
+        pending_approval: false
+      }
+    })
 
-        return res.status(200).json({ result: OrphanageView.renderMany(fetchedOrphanages) })
+    return res.status(200).json({ result: OrphanageView.renderMany(fetchedOrphanages) })
+  }
+
+  static async findById(req: Request, res: Response) {
+    const { id } = req.params
+
+    const orphanagesRepository = getRepository(OrphanageModel)
+
+    const fetchedOrphanage = await orphanagesRepository.findOne(id, {
+      relations: ["images"]
+    })
+
+    if (fetchedOrphanage) {
+      return res.status(200).json({
+        found: true,
+        result: OrphanageView.render(fetchedOrphanage)
+      })
+    } else {
+      return res.status(404).json({
+        found: false,
+        message: "No orphanage found with given ID."
+      })
+    }
+  }
+
+  static async create(req: Request, res: Response) {
+    const {
+      name,
+      about,
+      latitude,
+      longitude,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      whatsapp
+    } = req.body
+
+    const parameters: { [key: string]: any } = {
+      name,
+      about,
+      latitude,
+      longitude,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      whatsapp
     }
 
-    static async findById(req: Request, res: Response) {
-        const { id } = req.params
+    const missingParameters: string[] = []
 
-        const orphanagesRepository = getRepository(OrphanageModel)
-
-        const fetchedOrphanage = await orphanagesRepository.findOne(id, {
-            relations: ["images"]
-        })
-
-        if (fetchedOrphanage) {
-            return res.status(200).json({
-                found: true,
-                result: OrphanageView.render(fetchedOrphanage)
-            })
-        } else {
-            return res.status(404).json({
-                found: false,
-                message: "No orphanage found with given ID."
-            })
-        }
+    for (const param in parameters) {
+      if (!parameters[param]) {
+        missingParameters.push(param)
+      }
     }
 
-    static async create(req: Request, res: Response) {
-        const {
-            name,
-            about,
-            latitude,
-            longitude,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        } = req.body
-
-        const orphanagesRepository = getRepository(OrphanageModel)
-
-        const requestImages = req.files as Express.Multer.File[]
-
-        const images = requestImages.map(image => ({
-            path: image.filename
-        }))
-
-        const data = {
-            name,
-            about,
-            latitude,
-            longitude,
-            instructions,
-            opening_hours,
-            open_on_weekends: open_on_weekends === "true" ? true : false,
-            pending_approval: true,
-            images
-        }
-
-        const schema = yup.object().shape({
-            name: yup.string().required(),
-            about: yup.string().required().max(300),
-            latitude: yup.number().required(),
-            longitude: yup.number().required(),
-            instructions: yup.string().required(),
-            opening_hours: yup.string().required(),
-            open_on_weekends: yup.boolean().required(),
-            images: yup.array(
-                yup.object().shape({
-                    path: yup.string().required()
-                })
-            )
-        })
-
-        await schema.validate(data, { abortEarly: false })
-
-        const createdOrphanage = orphanagesRepository.create(data)
-
-        await orphanagesRepository.save(createdOrphanage)
-
-        return res.status(200).json({
-            status: 200,
-            type: "Post successfull",
-            message: "Orphanage created successfully.",
-            debug: createdOrphanage
-        })
+    if (missingParameters.length > 0) {
+      return res.status(400).json({
+        status: 400,
+        message: "The following parameters are missing: "
+          + missingParameters.join(", ")
+      })
     }
+
+    const orphanagesRepository = getRepository(OrphanageModel)
+
+    const requestImages = req.files as Express.Multer.File[]
+
+    const images = requestImages.map(image => ({
+      path: image.filename
+    }))
+
+    const data = {
+      name,
+      about,
+      latitude,
+      longitude,
+      instructions,
+      opening_hours,
+      open_on_weekends: open_on_weekends === "true" ? true : false,
+      pending_approval: true,
+      images,
+      whatsapp: Number(whatsapp)
+    }
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      about: yup.string().required().max(300),
+      latitude: yup.number().required(),
+      longitude: yup.number().required(),
+      instructions: yup.string().required(),
+      opening_hours: yup.string().required(),
+      open_on_weekends: yup.boolean().required(),
+      images: yup.array(
+        yup.object().shape({
+          path: yup.string().required()
+        })
+      ),
+      whatsapp: yup.number().required()
+    })
+
+    try {
+      await schema.validate(data, { abortEarly: false })
+    } catch (err) {
+      return res.status(400).json({
+        status: 400,
+        message: err.message
+      })
+    }
+
+    const createdOrphanage = orphanagesRepository.create(data)
+
+    await orphanagesRepository.save(createdOrphanage)
+
+    return res.status(200).json({
+      status: 200,
+      type: "Post successfull",
+      message: "Orphanage created successfully.",
+      debug: createdOrphanage
+    })
+  }
 }
