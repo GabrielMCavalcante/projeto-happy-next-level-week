74fb15b1f160251b37d0b0e2e04ddcf87b96ebc1
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-20 10:28:57 -0400

    Create index method

--- backend/src/controllers/dashboard.ts
+++ backend/src/controllers/dashboard.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from "express"
+import { getRepository } from "typeorm"
+import OrphanageModel from "../models/Orphanage"
+import DashboardView from "../views/dashboard"
+
+async function fetchOrphanages(pending_approval: boolean) {
+  const orphanagesRepository = getRepository(OrphanageModel)
+
+  const result = await orphanagesRepository.find({
+    where: { pending_approval }
+  })
+
+  return result
+}
 
 export default class DashboardController {
   static async index(req: Request, res: Response) {
+    const result = await fetchOrphanages(true)
 
+    console.log(result)
+
+    return res.status(200).json({
+      status: 200,
+      results: DashboardView.renderMany(result)
+    })
   }
 
   static async indexPending(req: Request, res: Response) {
