7493375c0425d531f2334559f0e86fced83803ff
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-20 10:14:22 -0400

    Simplified get routes

--- backend/src/routes/dashboard.ts
+++ backend/src/routes/dashboard.ts
@@ -6,8 +6,8 @@ import ParamsMiddleware from "../middlewares/checkEmptyOrNullParams"
 const routes = Router()
 const baseURL = "/dashboard"
 
-routes.get(`${baseURL}/orphanages`, AuthMiddleware, Controller.index)
-routes.get(`${baseURL}/orphanages/pending`, AuthMiddleware, Controller.indexPending)
+routes.get(`${baseURL}`, AuthMiddleware, Controller.index)
+routes.get(`${baseURL}/pending`, AuthMiddleware, Controller.indexPending)
 routes.put(`${baseURL}/update-orphanage/:id`, ParamsMiddleware, AuthMiddleware, Controller.update)
 routes.put(`${baseURL}/update-pending/:id`, ParamsMiddleware, AuthMiddleware, Controller.updatePending)
 routes.delete(`${baseURL}/orphanage/:id`, AuthMiddleware, Controller.delete)
