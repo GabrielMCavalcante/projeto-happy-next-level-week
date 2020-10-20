460748a45669a6edff0794c3219885df425afa8a
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-20 15:04:23 -0400

    Add multer config

--- backend/src/routes/dashboard.ts
+++ backend/src/routes/dashboard.ts
@@ -1,15 +1,21 @@
 import { Router } from "express"
+import multer from "multer"
+import multerConfig from "../config/upload"
 import Controller from "../controllers/dashboard"
 import AuthMiddleware from "../middlewares/AuthMiddleware"
 import ParamsMiddleware from "../middlewares/checkEmptyOrNullParams"
 
+const upload = multer(multerConfig)
 const routes = Router()
 const baseURL = "/dashboard"
 
 routes.get(`${baseURL}`, AuthMiddleware, Controller.index)
 routes.get(`${baseURL}/pending`, AuthMiddleware, Controller.indexPending)
-routes.put(`${baseURL}/update-orphanage/:id`, ParamsMiddleware, AuthMiddleware, Controller.update)
-routes.put(`${baseURL}/update-pending/:id`, ParamsMiddleware, AuthMiddleware, Controller.updatePending)
+routes.put(`${baseURL}/update-orphanage/:id`,
+  ParamsMiddleware,
+  AuthMiddleware,
+  upload.array("images"),
+  Controller.update)
 routes.delete(`${baseURL}/orphanage/:id`, AuthMiddleware, Controller.delete)
 
 export default routes
