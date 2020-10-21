ceaa6896536c33fa40245f3bb88b7b7c6c40ed8e
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-21 16:45:00 -0400

    Add auth context provider

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { AuthProvider } from "contexts/auth"
 
 // Styles
-import 'assets/styles/global.css'
-import 'leaflet/dist/leaflet.css'
+import "assets/styles/global.css"
+import "leaflet/dist/leaflet.css"
 
 const app = (
-  <App />
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 )
 
-ReactDOM.render(app, document.getElementById('root'))
+ReactDOM.render(app, document.getElementById("root"))
