a58472806ad175e7d42cb0727e65c6c9960f4e0f
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-21 08:21:44 -0400

    Create Feedback component

---
+++ web/src/components/Feedback.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+// Images
+import FeedbackSuccessImg from "assets/images/feedback-success.svg"
+import FeedbackErrorImg from "assets/images/feedback-error.svg"
+
+// Styles
+import "assets/styles/components/feedback.css"
+
+interface ActionButton {
+  type: "success" | "danger",
+  label: string,
+  action: () => void
+}
+
+interface FeedbackProps {
+  type: "success" | "error",
+  title: string,
+  subtitle: string,
+  actionButtons: ActionButton[]
+}
+
+const Feedback: React.FC<FeedbackProps> = (props) => {
+  const { type, title, subtitle, actionButtons } = props
+
+  return (
+    <div className={`feedback ${type}`}>
+      <div className="feedback-content">
+        <main>
+          <h1>{title}</h1>
+          <h3>{subtitle}</h3>
+
+          <div className="action-buttons-container">
+            {
+              actionButtons.map(button => (
+                <button
+                  onClick={button.action}
+                  className={`action-btn ${button.type}`}
+                >
+                  {button.label}
+                </button>
+              ))
+            }
+          </div>
+        </main>
+
+        <img
+          src={type === "success" ? FeedbackSuccessImg : FeedbackErrorImg}
+          alt="Feedback"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Feedback
