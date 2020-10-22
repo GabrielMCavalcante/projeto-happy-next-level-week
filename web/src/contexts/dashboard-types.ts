d4109f7e638a93b1bd277bad8f09b3f3f61d4966
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-22 14:36:49 -0400

    Add OrphanageCard and FetchOrphanagesData types

--- web/src/contexts/dashboard-types.ts
+++ web/src/contexts/dashboard-types.ts
@@ -6,3 +6,23 @@ export interface DashboardContextValues {
   deleteOrphanage: (id: number) => Promise<any>,
   loading: boolean
 }
+
+export interface OrphanageCard {
+  id: number,
+  name: string,
+  latitude: number,
+  longitude: number
+}
+
+interface FetchOrphanagesSuccessData {
+  status: 200,
+  result: OrphanageCard[]
+}
+
+interface FetchOrphanagesFailureData {
+  status: 401 | 500,
+  message: string
+}
+
+export type FetchOrphanagesData = 
+  FetchOrphanagesSuccessData | FetchOrphanagesFailureData
