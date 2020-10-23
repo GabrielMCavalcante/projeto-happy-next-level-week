26d85025b2af5011dd2dcab0c0fc119cc645c832
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-10-23 17:02:36 -0400

    Add import for NoOrphanageFaceLogo

--- web/src/utils/images.ts
+++ web/src/utils/images.ts
@@ -1,9 +1,11 @@
 import happyFaceLogo from '../assets/images/happy-face-logo.svg'
 import landingKidsImg from '../assets/images/landing-kids.svg'
 import appLogo from '../assets/images/logo.svg'
+import NoOrphanageFaceLogo from "../assets/images/no-orphanage.svg"
 
 export {
     happyFaceLogo,
     landingKidsImg,
-    appLogo
+    appLogo,
+    NoOrphanageFaceLogo
 }
